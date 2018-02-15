872af02f9bce7936c27d3716e5ad878d3a4c8c4e
otterbeck <otterbeck@sdaq.physik.uni-wuppertal.de>
2018-02-15 20:40:50 +0100

    new features: adding dynamically tabs with baseadress and table. Now add and remove rows in tables is possible. Table now uses all space in horizontal direction. Tabs can also be removed, except the first one, and the + tab. Last column of table is checkable, but the boxes are on the left. It seems to extensive move them to the middle. Next step is create validator for IP-Adresses and a load/save function.

--- mainwindow.h
+++ mainwindow.h
@@ -5,9 +5,13 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QRegExp>
+#include <QSizePolicy>
 #include <QStandardItemModel>
 #include <QTableView>
 #include <QTabWidget>
+#include <QValidator>
 
 #include <iostream>
 
@@ -23,10 +27,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void addRow();
+    void addTab(int i);
+    void removeRow();
+    void removeTab(int i);
+    void clickAll();
+
 private:
     Ui::MainWindow *ui;
-    QStandardItemModel model;
-    QTableView view;
+    //QStandardItemModel *model = new QStandardItemModel;
+    //QTableView *view = new QTableView;
+    QTabWidget *tabWidget = new QTabWidget;
+   // QPushButton *addbutton;
+
+
+
+
 };
 
 #endif // MAINWINDOW_H
