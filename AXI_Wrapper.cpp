1522ea14dfe837855da9821c27026f96f2f925af
Marvin Geyik <marvin.emin.geyik@cern.ch>
2018-04-23 16:32:21 +0200

    CHANGES: 	- AXI_Wrapper.h zu ausschließlicher Header-Datei umfunktioniert. Eigentliche Methoden stehen in neuer Datei AXI_Wrapper.cpp.
    ADDED:
    	- AXI_Wrapper.cpp. Datei beinhaltet Methoden des AXI_Wrappers. Klasse funktioniert nun wie folgt:
    		> Objekt der Klasse AXI_Wrapper deklarieren. Der Konstruktor initialisiert die wichtigsten Variablen.
    		> Zwei Möglichkeiten zur Initialisierung des Mappings: Init(uint32_t baseAddr) und Init_Emu(uint32_t baseAddr). Init_Emu() initialisiert einen Emulator. Sinnvoll, falls kein Zugriff auf /dev/mem besteht.
    		  Init() initialisiert das tatsächliche Mapping ab der base-Addresse.
    		> Methode uint32_t read(uint32_t offset) liest den Wert der Speicherzelle an der Stelle Offset aus. (Funktioniert nach beiden Initialisierungsmethoden gleich)
    		> Methode int write(uint32_t offset, uint32_t value) schreibt den Wert value in die Speicherzelle an der Stelle offset. (Fuktioniert nach beiden Initialisierungsmethoden gleich)
    		> Methode uint32_t getBaseAddr() liefert Base Addresse
    		> Methode bool is_mapped() liefert true, falls ein tatsächliches mapping (kein emuliertes) vorhanden ist
    		> Methode bool dev_mem_open() liefert true, wenn /dev/mem geöffnet ist

---
+++ AXI_Wrapper.cpp
@@ -0,0 +1,176 @@
+#include "AXI_Wrapper.h"
+
+
+	AXI_Wrapper::AXI_Wrapper(void)
+    {
+		emu = false;
+		fd_open = false;
+		mapped = false;
+		base = 0x00000000;
+		MappedBase = NULL;
+		MappedBase32 = NULL;
+		Emu_data.reserve(32*1024);
+	}
+
+
+	int AXI_Wrapper::openDevMem(void)
+	{
+		int fd = open("/dev/mem",O_RDWR|O_SYNC);
+		fd_open = true;
+		if(fd < 0)
+		{
+			std::cout << "Error in AXI_Wrapper::openDevMem(void): Can't open /dev/mem\n";
+			fd_open = false;
+		}
+		return fd;
+	}
+
+
+	int AXI_Wrapper::Init(uint32_t baseAddr)
+	{
+		emu = false;
+		if(mapped)
+		{
+			munmap(MappedBase, 128*1024);
+			mapped = false;
+		}
+		
+		base = baseAddr;
+		fd = openDevMem();
+		if(fd_open)
+		{
+			MappedBase = mmap(NULL, 128*1024, PROT_READ|PROT_WRITE, MAP_SHARED, fd, base);
+			MappedBase32 = (uint32_t *) MappedBase;
+		}
+		else
+		{
+			std::cout << "Error in AXI_Wrapper::Init(uint32_t baseAddr): Can't map, because /dev/mem is not opened." << std::endl;
+			return 1;
+		}
+
+		if(MappedBase == NULL)
+		{
+			std::cout << "Error in AXI_Wrapper::Init(uint32_t baseAddr): Can't Map.\n";
+			mapped = false;
+			return 1;
+		}
+		else
+		{
+			std::cout << "Mapping was successful. Or there was an unknown error, in which case: RIP.\n" << std::endl; //Debugging comment
+			mapped = true;
+			return 0;
+		}
+	}
+
+
+	int AXI_Wrapper::Init_Emu(uint32_t baseAddr)
+	{
+		emu = true;
+		if(mapped)
+		{
+			munmap(MappedBase, 128*1024);
+			mapped = false;
+		}
+		for(int i = 0; i < 1024*32; i++)
+		{
+			Emu_data.push_back(0x12345678);
+		}
+		base = baseAddr;
+		return 0;
+	}
+
+
+	int AXI_Wrapper::unmap(void)
+	{
+		if(mapped)
+		{
+			if(MappedBase == NULL)
+			{
+				std::cout << "Warning in AXI_Wrapper::unmap(void): Can't unmap, because there is no mapping." << std::endl;
+			}
+			else
+			{
+				munmap(MappedBase, 128*1024);
+			}			
+			mapped = false;
+		}
+		emu = false;
+		return 0;
+	}
+
+
+	uint32_t AXI_Wrapper::read(uint32_t offset)
+	{
+		if(offset > 128*1024)
+		{
+			std::cout << "Error in AXI_Wrapper::read(uint32_t offset): Offset ist larger than the mapped area. Returning standard value 0x00000000." << std::endl;
+		}
+	
+		uint32_t value;
+		if(mapped)
+		{
+			value = *(MappedBase32 + (((unsigned int)offset)>>2));
+		}
+		else if(emu) 
+		{
+			value = Emu_data.at(((unsigned int)offset)>>2);
+		}
+		else 
+		{
+			std::cout << "Error in AXI_Wrapper::read(uint32_t offset): No Mapping initialized. Returning standard value 0x00000000." << std::endl;
+			value = 0;
+		}
+		return value;
+	}
+
+
+	int AXI_Wrapper::write(uint32_t offset, uint32_t value)
+	{
+		if(offset > 128*1024)
+		{
+			std::cout << "Error in AXI_Wrapper::write(uint32_t offset, uint32_t value): Offset ist larger than the mapped area." << std::endl;
+			return 1;
+		}
+
+		if(mapped)
+		{
+			*(MappedBase32 + (((unsigned int)offset)>>2)) = value;
+			if(*(MappedBase32 + (((unsigned int)offset)>>2)) == value)
+			{
+				return 0;
+			}
+			else
+			{
+				std::cout << "Warning in AXI_Wrapper::write(uint32_t offset, uint32_t value): Address did fully not assume the given value. The assumed value is " << std::hex << *(MappedBase32 + (((unsigned int)offset)>>2)) << "." << std::dec << std::endl;
+				return 2;
+			}
+		}
+		else if(emu)
+		{
+			Emu_data.at(((unsigned int)offset)>>2) = value;
+			return 0;
+		}
+		else 
+		{
+			std::cout << "Error in AXI_Wrapper::write(uint32_t offset, uint32_t value): No Mapping initialized." << std::endl;
+			return 1;
+		}
+	}
+
+
+	uint32_t AXI_Wrapper::getBaseAddr(void)
+	{
+		return base;
+	}
+
+	
+	bool AXI_Wrapper::is_mapped(void)
+	{
+		return mapped;
+	}
+	
+	
+	bool AXI_Wrapper::dev_mem_open(void)
+	{
+		return fd_open;
+	}
--- AXI_Wrapper.h
+++ AXI_Wrapper.h
@@ -1,62 +1,38 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <vector>
+
 class AXI_Wrapper
 {
         private:
                 uint32_t base;
-                uint32_t *MappedBase;
+				uint32_t *MappedBase32;
+                void *MappedBase;
+				std::vector<uint32_t> Emu_data;
                 int fd;
+				bool emu, fd_open, mapped;
 
-
-                int openDevMem(void)
-                {
-                        int fd = open("/dev/mem",O_RDWR|O_SYNC);
-                        if(fd < 0)
-                        {
-                                printf("Can't open /dev/mem\n");
-                        }
-                        return fd;
-                }
+				int openDevMem(void);
 
         public:
-                AXI_Wrapper(uint32_t baseAddr)
-                {
-                        base = baseAddr;
-                        int fd = openDevMem();
-                        MappedBase = (uint32_t *) mmap(NULL, 256*1024, PROT_READ|PROT_WRITE, MAP_SHARED, fd, base);
-                        if(MappedBase == NULL)
-                        {
-                                printf("Can't mmap :(\n");
-                        }
-                }
+                AXI_Wrapper();
+                
+				int Init(uint32_t baseAddr);
+				int Init_Emu(uint32_t baseAddr);
+				int unmap();
+				
+                uint32_t read(uint32_t offset);
+                int write(uint32_t offset, uint32_t value);
 
-                uint32_t read(uint32_t offset)
-                {
-                        uint32_t value = *(MappedBase + (((unsigned int)offset)>>2));
-                        return value;
-                }
-
-                int write(uint32_t offset, uint32_t value)
-                {
-                        *(MappedBase + (((unsigned int)offset)>>2)) = value;
-                        if(*(MappedBase + (((unsigned int)offset)>>2)) == value)
-                        {
-                                return 0;
-                        }
-                        else
-                        {
-                                printf("Warning: Address did fully not assume the given value.\n");
-                                return 1;
-                        }
-                }
-
-                uint32_t getBaseAddr(void)
-                {
-                        return base;
-                }
-
-                int setBaseAddr(uint32_t baseAddr)
-                {
-                        base = baseAddr;
-                        return 0;
-                }
+                uint32_t getBaseAddr(void);
+				bool is_mapped(void);
+				bool dev_mem_open(void);
 };
 
